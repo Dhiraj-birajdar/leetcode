dc1fea84d7b14b0567e576b4ea9c294ef580959e
Dhiraj Birajdar <114354331+Dhiraj-birajdar@users.noreply.github.com>
2023-08-29 17:37:08 +0530

    Update 0013-roman-to-integer.py (2 solved methods)

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -18,6 +18,44 @@ class Solution:
         if rToI[s[len(s)-1]] > rToI[s[len(s)-2]]:
             pass
         else:
-            res+=rToI[s[len(s)-1]]
+            res+=rToI[s[-1]]
         
         return res
+""" methond 1
+class Solution(object):
+    def romanToInt(self, s):
+        roman = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000
+        }
+        total = 0
+        for i in range(len(s) - 1):
+            if roman[s[i]] < roman[s[i+1]]:
+                total -= roman[s[i]]
+            else:
+                total += roman[s[i]]
+        return total + roman[s[-1]]
+"""
+""" methond 2
+        roman = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000
+        }
+        total = 0
+        s = s.replace("IV", "IIII").replace("IX", "VIIII")
+        s = s.replace("XL", "XXXX").replace("XC", "LXXXX")
+        s = s.replace("CD", "CCCC").replace("CM", "DCCCC")
+        for symbol in s:
+            total += roman[symbol]
+        return total
+"""
