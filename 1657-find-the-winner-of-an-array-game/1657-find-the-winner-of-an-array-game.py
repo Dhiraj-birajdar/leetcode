e86e5ca8b07e68613df6c95c2f2d5b621c0b10da
Dhiraj Birajdar <114354331+Dhiraj-birajdar@users.noreply.github.com>
2023-11-05 16:03:35 +0530

    Time: 4788 ms (5.43%), Space: 30.2 MB (35.87%) - LeetHub

--- 1657-find-the-winner-of-an-array-game/1657-find-the-winner-of-an-array-game.py
+++ 1657-find-the-winner-of-an-array-game/1657-find-the-winner-of-an-array-game.py
@@ -1,6 +1,7 @@
 class Solution:
     def getWinner(self, arr: List[int], k: int) -> int:
         w = 0
+        l = len(arr)
         while(True):
             if arr[0] > arr[1]:
                 arr.append(arr.pop(1))
@@ -9,5 +10,5 @@ class Solution:
                 arr.append(arr[0])
                 arr.pop(0)
                 w = 1
-            if w== len(arr) or w == k:
+            if w == l or w == k:
                 return arr[0]
