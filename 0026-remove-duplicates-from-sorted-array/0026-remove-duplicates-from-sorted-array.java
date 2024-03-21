3a51977d5cc46c034410cec1cf746ba42526d7d5
Dhiraj Birajdar <114354331+Dhiraj-birajdar@users.noreply.github.com>
2024-03-21 20:33:55 +0530

    Time: 1 ms (74.80%), Space: 44.7 MB (40.39%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -1,11 +1,20 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        Set <Integer> s = new LinkedHashSet();
-    	for(int e: nums)
-    		s.add(e);
-    	int i = 0;
-    	for(Integer e: s)
-    		nums[i++] = e;
-    	return s.size();
+        int j = 1;
+       for(int i = 1; i<nums.length;i++) {
+    	   if(nums[i] != nums[i-1]) {
+    		   nums[j] = nums[i];
+    		   j++;
+    	   }
+       }
+       return j;
+
+        // Set <Integer> s = new LinkedHashSet();
+    	// for(int e: nums)
+    	// 	s.add(e);
+    	// int i = 0;
+    	// for(Integer e: s)
+    	// 	nums[i++] = e;
+    	// return s.size();
     }
 }
