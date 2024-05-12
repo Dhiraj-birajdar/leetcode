e08d382143b58df783caec7800509af870bf2faf
Dhiraj Birajdar <114354331+Dhiraj-birajdar@users.noreply.github.com>
2024-05-12 23:40:12 +0530

    Time: 1 ms (95.67%), Space: 45.4 MB (46.12%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.java
+++ 0605-can-place-flowers/0605-can-place-flowers.java
@@ -5,27 +5,27 @@ class Solution {
         if(n > Math.ceil(flowerbed.length/2.0)) return false;
         if(flowerbed.length == 1 && flowerbed[0] == 0 && n ==1) return true;
         
-        int i = 0, c = 0;
+        int i = 0;
         
         if(flowerbed[0] == 1) 
             i+=2;
         else if(flowerbed[0] == 0 && flowerbed[1] == 0){
             flowerbed[0] = 1;
-            c++;
+            n--;
             i = 1;
         }else if(flowerbed[0] == 0 && flowerbed[1] == 1) i=3;
 
-        while(i < flowerbed.length-1){
+        while(i < flowerbed.length-1 && n>0){
             if(flowerbed[i] == 0 && (flowerbed[i-1]==0 && flowerbed[i+1]==0)){
                 flowerbed[i] = 1;
-                c++;
+                n--;
                 i++;
             }else
                 i++;
         }
         
-        if(flowerbed[flowerbed.length-1]==0 && flowerbed[flowerbed.length-2] == 0) c++;
+        if(flowerbed[flowerbed.length-1]==0 && flowerbed[flowerbed.length-2] == 0) n--;
         
-        return n<=c? true: false;
+        return n<=0? true: false;
     }
 }
