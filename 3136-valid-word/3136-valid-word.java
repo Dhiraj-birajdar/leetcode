5c596fda3ec43be9c75fc0e17e7f3bd90bb96e12
Dhiraj Birajdar <114354331+Dhiraj-birajdar@users.noreply.github.com>
2024-06-19 12:38:56 +0530

    Time: 13 ms (5.47%), Space: 43.3 MB (5.67%) - LeetHub

--- 3136-valid-word/3136-valid-word.java
+++ 3136-valid-word/3136-valid-word.java
@@ -1,12 +1,13 @@
 class Solution {
     public boolean isValid(String word) {
         if(word.length()<3 || !word.matches("[a-zA-Z0-9]+")) return false;
+        return word.matches("(?i).*[aeiou].*") && word.matches("(?i).*[^aeiou0-9].*");
 
-        boolean vowel = false;
-        boolean consonant = false;
+//         boolean vowel = false;
+//         boolean consonant = false;
 
-        vowel = word.toLowerCase().matches(".*[aeiou].*");
-        consonant = word.toLowerCase().matches(".*[^aeiou0-9].*");
-        return vowel && consonant;
+//         vowel = word.toLowerCase().matches(".*[aeiou].*");
+//         consonant = word.toLowerCase().matches(".*[^aeiou0-9].*");
+        // return vowel && consonant;
     }
 }
