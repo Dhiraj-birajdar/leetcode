25531aaa7b82a40fc0236ee4a5f9f4392fae86fe
Dhiraj Birajdar <114354331+Dhiraj-birajdar@users.noreply.github.com>
2024-07-31 22:02:26 +0530

    Time: 77 ms (52.76%), Space: 46.2 MB (48.89%) - LeetHub

---
+++ 2470-removing-stars-from-a-string/2470-removing-stars-from-a-string.java
@@ -0,0 +1,20 @@
+class Solution {
+    public String removeStars(String s) {
+        Stack<Character> stack = new Stack<>();
+        for(char c: s.toCharArray()){
+            if(c=='*')
+                stack.pop();
+            else
+                stack.push(c);
+        }
+        StringBuilder sb = new StringBuilder();
+        while(!stack.isEmpty())
+            sb.append(stack.pop());
+        return sb.reverse().toString();
+       /* return stack.toString()
+            .replaceAll(" ","")
+            .replaceAll(",","")
+            .replace("[","")
+            .replace("]","");*/
+    }
+}
