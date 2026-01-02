cdb0af610e065fd5e26993c1f1bd5519935097b2
Dhiraj Birajdar <114354331+Dhiraj-birajdar@users.noreply.github.com>
2026-01-02 22:45:33 +0530

    Time: 5 ms (78.23%), Space: 77.3 MB (68.23%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -1,17 +1,14 @@
 class Solution {
     public int maxArea(int[] height) {
-        
-        if(height.length == 2) return Math.min(height[0],height[1]);
-
-        int l = 0, r = height.length-1, res = (r-l)*Math.min(height[l],height[r]);
-        while(l<r){
-            if(height[l] <= height[r]){
-                l++;
-            }else{
-                r--;
-            }
-            res = Math.max(res, (r-l)*Math.min(height[l],height[r]));
-        }        
-        return res;
+        // if(height.length == 2) return Math.min(height[0],height[1]);
+        int maxCapacity = 0;
+        for(int l=0,r=height.length-1;l<r;){
+            int currentCapacity = (r-l)*Math.min(height[l],height[r]);
+            maxCapacity = Math.max(currentCapacity,maxCapacity);
+            if(height[l]<=height[r]) l++;
+            else if(height[l]>height[r]) r--;
+        }
+        return maxCapacity;
     }
 }
+//use two-pointers. Set one pointer to the left and one to the right of the array. Always move the pointer that points to the lower line.
